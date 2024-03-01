9332d0cdcec77fa2d67ef0ec6614aaa608ee56e4
Oliver Giczi <117094162+oleeey@users.noreply.github.com>
2024-03-01 16:23:49 +0100

    Update iss-tracker.py

--- iss-tracker.py
+++ iss-tracker.py
@@ -1,1 +1,16 @@
+from urllib.request import urlopen
+import time, requests
+
+def getData():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    longitude = response.json()["iss_position"]["longitude"]
+    latitude = response.json()["iss_position"]["latitude"]
+    return longitude, latitude
+
+while True:
+    print(getData())
+    time.sleep(1)
+
+
+
 
